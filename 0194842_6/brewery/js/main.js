fca13e4a8f3b42b3adb490a8ada0f95565fff9de
Jezz03 <0194842@up.edu.mx>
2020-05-24 22:41:34 -0700

    Update

--- 0194842_6/brewery/js/main.js
+++ 0194842_6/brewery/js/main.js
@@ -1,19 +1,20 @@
 /*
 *    main.js
 */
-//Exersize 6
-var width = 600;
-var height = 400;
-var margin = {left:100, right: 10, top: 10, bottom: 100};
+//EXERCISE 6
 
-var g = d3.select("chart-area").append("svg")
+var margin = {left:100, right: 10, top: 10, bottom: 100};
+var width = 600 - margin.left - margin.right;
+var height = 400 - margin.top - margin.bottom;
+var flag = true;
+var g = d3.select("#chart-area").append("svg")
 .attr("width", width + margin.right + margin.left)
 .attr("height", height + margin.top + margin.bottom)
 .append("g")
 .attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
 
 var x = d3.scaleBand().range([0,width]).padding(0.2);
-var y = d3.scaleLinear().range([0,height]);
+var y = d3.scaleLinear().range([height,0]);
 
 var xAxisGroup = g.append("g").attr("class", "x axis")
   .attr("transform", "translate(0, " + height + ")");
@@ -27,7 +28,7 @@ g.append("text")
 .attr("text-anchor", "middle")
 .text("Month");
 // Y Label
-g.append("text")
+var yLabel = g.append("text")
 .attr("class", "y axis-label")
 .attr("x", - (height / 2))
 .attr("y", -60)
@@ -37,47 +38,37 @@ g.append("text")
 .text("Revenue (dlls.)");
 d3.json("data/revenues.json").then((data) => {data.forEach((d, i)=>{
         d.revenue = +d.revenue;
+        d.profit = +d.profit;
       });
       d3.interval( ( )=>{update(data)}, 1000);
       update(data);
+      flag = !flag;
   }).catch((error)=>{console.log(error);
   });
 function update(data){
-
+  var value = flag ? "revenue" : "profit";
+  var label = flag ? "Revenue" : "Profit";
+  yLabel.text(label)
   x.domain(data.map((d)=>{ return d.month; }));
-  y.domain([0, d3.max(data, (d) => { return d.revenue; })]);
+  y.domain([0, d3.max(data, (d) => { return d[value]; })]);
   var bottomAxis = d3.axisBottom(x);
   var leftAxis = d3.axisLeft(y).ticks(11).tickFormat(d3.format("$.0s"));
-  xAxisGroup.call(bottomAxis)
-    .selectAll("text")
-    .attr("y", 10)
-    .attr("x", 0)
-    .attr("text-anchor", "middle");
+  xAxisGroup.call(bottomAxis);
   yAxisGroup.call(leftAxis);
   var months = data.map((d) => {return d.month;});
   var revenues = data.map((d) => {return d.revenue;});
   var max = Math.max.apply(null, revenues);
 
-  /*g.append("g")
-  .attr("class", "x axis")
-  .attr("transform", "translate(0, " + height + ")")
-  .call(bottomAxis)
-  .selectAll("text")
-  .attr("y", 10)
-  .attr("x", 0)
-  .attr("text-anchor", "middle");*/
-
-
-  /*g.append("g")
-  .attr("class", "left axis")
-  .call(leftAxis);*/
-
-  var bars = g.selectAll("rect").data(data)
-    .enter()
-    .append("rect")
+  var bars = g.selectAll("rect").data(data);
+  bars.exit().remove();
+  bars.attr("x", (d) => {return x(d.month);})
+    .attr("y", (d)=>{return y(d[value]);})
+    .attr("width", x.bandwidth())
+    .attr("height", (d)=>{return height - y(d[value]);});
+  bars.enter().append("rect")
     .attr("x", (d) => {return x(d.month);})
-    .attr("y", (d)=>{return y(d.revenue);})
+    .attr("y", (d)=>{return y(d[value]);})
     .attr("width", x.bandwidth())
     .attr("height", (d)=>{return height - y(d.revenue);})
-    .attr("fill","yellow");
+    .attr("fill", "yellow");
 }
